390a33cf72ae2581467a74a51d3394261f2d9c17
Harita Reddy <harita_1997@yahoo.com>
2018-09-20 15:26:33 +0530

    Sorted into Folders

--- Fake_News_Detection/train_test_split.py
+++ Fake_News_Detection/train_test_split.py
@@ -5,7 +5,7 @@ from sklearn.metrics import confusion_matrix, precision_recall_fscore_support, r
 import pickle
 import sys
 
-tsv = 'fakenewsnet.txt'
+tsv = 'FakeNewsNet_Dataset/fakenewsnet.txt'
 f=open(tsv,'r')
 x=[]
 y=[]
@@ -19,12 +19,12 @@ for line in f:
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
 
-p1=open('fakenewsnet_train.txt','w')
+p1=open('FakeNewsNet_Dataset/fakenewsnet_train.txt','w')
 
 for i in range(0,len(x_train)):
 	p1.write(x_train[i]+'\t'+str(y_train[i])+'\n')
 
-p2=open('fakenewsnet_test.txt','w')
+p2=open('FakeNewsNet_Dataset/fakenewsnet_test.txt','w')
 
 for i in range(0,len(x_test)):
 	p2.write(x_test[i]+'\t'+str(y_test[i])+'\n')
