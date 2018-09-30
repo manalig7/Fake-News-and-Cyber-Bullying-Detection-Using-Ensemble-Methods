900111cfc6fba2adc264dd727552303e412221cc
Harita Reddy <harita_1997@yahoo.com>
2018-09-30 13:56:27 +0530

    Syllable count, word count, sentence count

--- Fake_News_Detection/lying_detection_feature_set.py
+++ Fake_News_Detection/lying_detection_feature_set.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 tsv = 'FakeNewsNet_Dataset/fakenewsnet_train.txt'
 f=open(tsv,'r')
@@ -22,6 +23,63 @@ for i in range(0,len(x_train)):
 #x_train contains the list of pieces of news texts
 #1st feature- Quantity (number of syllables, number of words, number of sentences)
 
+import spacy 
+from textstat.textstat import easy_word_set, legacy_round 
+from textstat.textstat import textstatistics
+  
+ ##########################################FUNCTIONS################################# 
+def break_sentences(text): 
+    nlp = spacy.load('en') 
+    doc = nlp(text) 
+    return doc.sents 
+  
+# Returns Number of Words in the text 
+def word_count(text): 
+    sentences = break_sentences(text) 
+    words = 0
+    for sentence in sentences: 
+        words += len([token for token in sentence]) 
+    return words 
+  
+# Returns the number of sentences in the text 
+def sentence_count(text): 
+    sentences = break_sentences(text) 
+    return len(sentences) 
+
+def num_syllables(word): 
+    return textstatistics().syllable_count(word) 
+
+# Returns the average number of syllables per 
+# word in the text 
+def avg_syllables_per_word(text): 
+    syllable = num_syllables(text) 
+    words = word_count(text) 
+    ASPW = float(syllable) / float(words) 
+    return legacy_round(ASPW, 1) 
+
+##############################################FEATURES################################
+#Number of Words
+
+for i in range(0,len(x_train)):
+	text=x_train[i]
+	wordcount=word_count(text)
+	feature_set[i].append(wordcount)
+
+#Number of Sentences
+for i in range(0,len(x_train)):
+	text=x_train[i]
+	sentencecount=sentence_count(text)
+	feature_set[i].append(sentencecount)
+
+#Average Number of Syllables per word
+for i in range(0,len(x_train)):
+	text=x_train[i]
+	avg_syl=avg_syllables_per_word(text)
+	feature_set[i].append(avg_syl)
+
+
+
+"""
 #Number of Sentences
 for i in range(0,len(x_train)):
 	text=x_train[i]
@@ -50,3 +108,8 @@ for i in range(0,len(tokenized_text)):
 	for item in remove_list:
 		tokenized_text[i].remove(item)
 	feature_set[i].append(len(tokenized_text))
+
+"""
+#3rd Feature- Grammatical Complexity
+#Flesh-Kincaid Grade Level
+
