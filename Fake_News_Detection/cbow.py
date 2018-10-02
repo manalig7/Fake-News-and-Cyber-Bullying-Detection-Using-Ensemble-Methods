dc207669ce6ea2862fea67d6bee49aa4338ea1f2
Namratha <namratha.mraj@gmail.com>
2018-10-03 00:26:29 +0530

    Add files via upload

--- Fake_News_Detection/cbow.py
+++ Fake_News_Detection/cbow.py
@@ -6,6 +6,20 @@ from gensim.models import Word2Vec
 from gensim.models import FastText
 
 
+def fit_transform(x,d,op):
+	res=[]
+	for i in range(0,len(x_train)):
+		temp=[]
+		if op==0:
+			for word in d[i]:
+				print word
+				temp.append(np.mean(model_W2V.wv(word)))		
+		else:
+			for word in d[i]:
+				temp.append(np.mean(model_FT.wv(word)))
+		res.append(temp)
+	return res
+
 tsv = 'dataset/finaldataset_train.txt'
 f=open(tsv,'r')
 x_train=[]
@@ -25,12 +39,39 @@ for line in f :
 	data.append(temp) 
 	y_train.append(int(ls[1]))
 f.close()
-print(data)
+print(len(x_train))
 
-model_ted = Word2Vec(data, size=100, window=5, min_count=1, workers=5, sg=0)
+model_W2V = Word2Vec(data, size=100, window=5, min_count=1, workers=5, sg=0)
 
 
-model_ted = FastText(data, size=100, window=5, min_count=1, workers=5,sg=0)
+#model_FT = FastText(data, size=100, window=5, min_count=1, workers=5,sg=0)
 
-#def fit_transform(X):
-	
+print "model_done!"
+
+X_train=fit_transform(x_train,data,0)
+
+print(X_train)
+
+"""
+tsv1 = 'dataset/finaldataset_test.txt'
+f=open(tsv1,'r')
+x_test=[]
+y_test=[]
+
+data1=[]
+for line in f:
+	ls=line.split('\t')
+	x_test.append(ls[0])
+	temp = [] 
+	#print(ls[0])
+	for j in tokenizer.tokenize(ls[0].decode('utf-8')):
+		#print(j) 
+	       	temp.append(j) 
+	data1.append(temp) 
+	y_test.append(int(ls[1]))
+f.close()
+
+model_W2V.train(data1)
+model_FT.train(data1)
+
+X_test=fit_transform(x_test,data1,0)"""
