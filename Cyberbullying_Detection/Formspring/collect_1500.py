2c774a8e7fb7cb1589497b0637321047ffee2027
Harita Reddy <harita_1997@yahoo.com>
2019-01-16 10:38:18 +0530

    Selected 1500 in Formspring.me

---
+++ Cyberbullying_Detection/Formspring/collect_1500.py
@@ -0,0 +1,27 @@
+
+f=open('formspring_modified_2.tsv','r')
+
+text=[]
+labels=[]
+count=0
+maxi=776
+
+for line in f:
+	ls=line.split('\t')
+	if int(ls[1])==1:
+		text.append(ls[0])
+		labels.append(int(ls[1]))
+	else:
+		if count<maxi:
+			text.append(ls[0])
+			labels.append(int(ls[1]))
+			count=count+1
+
+f.close()
+
+g=open('selected_1500.txt','w')
+
+for i in range(0,len(labels)):
+	g.write(text[i]+'\t'+str(labels[i])+'\n')
+
+g.close()
