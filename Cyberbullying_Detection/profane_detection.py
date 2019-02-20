d8f39f8035b8bf49013810772bf6e9eed66d4e8b
Harita Reddy <harita_1997@yahoo.com>
2019-02-20 10:52:29 +0530

    Update profane_detection.py

--- Cyberbullying_Detection/profane_detection.py
+++ Cyberbullying_Detection/profane_detection.py
@@ -1,4 +1,31 @@
-
+import sys
 medium_words=[]
 strong_words=[]
 mild_words=[]
+
+
+f=open('ofcom_profane_strong.txt','r')
+
+for line in f:
+	ls=line.split()
+	strong_words.append(ls[0])
+
+f.close()
+
+
+f=open('ofcom_profane_medium.txt','r')
+
+for line in f:
+	ls=line.split()
+	medium_words.append(ls[0])
+
+f.close()
+
+
+f=open('ofcom_profane_mild.txt','r')
+
+for line in f:
+	ls=line.split()
+	mild_words.append(ls[0])
+
+f.close()
